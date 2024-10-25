ab41e57b798da7d07354f15c2cfe4f4ea9939cb8
gitzs-test <zhisin93@gmail.com>
2024-10-25 19:09:15 +0800

    Delete src/GamePlatform.java

--- src/GamePlatform.java
+++
@@ -1,9 +0,0 @@
-public class GamePlatform {
-    public static double getFinalSpeed(double initialSpeed, int[] inclinations) {
-
-        throw new UnsupportedOperationException("Waiting to be implemented.");
-    }
-    public static void main(String[] args){
-        System.out.println(getFinalSpeed(60.0, new int[] { 0, -30, 0, 45, 0 })); //should print 45
-    }
-}
